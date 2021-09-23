9760a39a20453f1fc85cca53e66f69f67cb9d996
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-23 01:22:17 -0400

    Adds more path/url validator logic

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,40 @@
 const request = require('request');
+const fs = require('fs');
+const isValid = require('is-valid-path');
+const readline = require('readline');
+
 const myArgs = process.argv.slice(2);
-const fs = require('fs')
+
+// Check if path is valid
+if (!isValid(myArgs[1])) {
+  console.log("File path invalid! Terminating...");
+  return;
+}
+
+// Check if URL is valid
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+  return true;
+};
+if (!isValidUrl(myArgs[0])) {
+  console.log("URL is not valid! Terminating...");
+  return;
+}
+
+// Check if file already exists
+fs.access(myArgs[1], fs.F_OK, (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  //file exists, ask to continue
+});
 
 // Establish a connection to the url
 request(myArgs[0], (error, response, body) => {
